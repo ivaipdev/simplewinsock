bf5c57eee7749dfb2f0739582ee031688d7ddbab
ivaipdev <ivaip.letter@gmail.com>
2024-06-24 19:18:34 +0300

    header file
    there added libs and all stuff

---
+++ server.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <WinSock2.h>
+#include <WS2tcpip.h>
+#include <iphlpapi.h>
+#include <Windows.h>
+#include <iostream>
+#include <string>
+#include <fstream>
+
+#pragma comment(lib, "Ws2_32.lib")
